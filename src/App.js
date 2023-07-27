2be217d2b7bfd657f008849d612904ad7efc99c5
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-26 23:52:23 -0400

    Completed adding chat engine to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <ChatEngine
       height="100vh"
-      
+      projectID="54d0dd34-ba12-4de2-aa8b-61ea06d593dc"
+      userName="Jack"
+      userSecret="12345"
       
       />
     </div>
