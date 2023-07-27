a979d23121724bc24b20ba7628a1a5ddf2146a23
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-27 00:07:45 -0400

    added components for my message, their message, message form, and chat feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { ChatEngine } from "react-chat-engine";
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       projectID="54d0dd34-ba12-4de2-aa8b-61ea06d593dc"
       userName="Jack"
       userSecret="12345"
-      
+      renderChatFeed = {(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+      onNewMessage = {()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     </div>
   );
