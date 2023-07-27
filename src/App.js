714754bf174ca507f4055f9e850f9e62755ae08d
Xavier Dunkley <xavierxv18@gmail.com>
2023-07-26 23:39:57 -0400

    Added css and react chat engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import './App.css';
+import { ChatEngine } from "react-chat-engine";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <ChatEngine
+      height="100vh"
+      
+      
+      />
     </div>
   );
 }
