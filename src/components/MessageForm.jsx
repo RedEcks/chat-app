6ed6d0311b0969bcdc7572c42e83e7a42a02517b
Xavier Dunkley <xavierxv18@gmail.com>
2023-08-05 13:19:23 -0400

    Added code for MessageForm

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,7 +1,65 @@
-const MessageForm = ()=>{
+import { useState } from "react";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons"
+import { sendMessage, isTyping, sendMessage } from "react-chat-engine";
+
+
+const MessageForm = (props)=>{
+
+    const [value, setValue] = useState('');
+    const { chatID, creds} = props;
+
+    const handleChange=(event) =>{
+        setValue(event.target.value)
+        isTyping(props, chatID)
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0){
+            sendMessage(creds,chatID,{text})
+        }
+        setValue('')
+    }
+
+    const handleUpload = (event) =>{
+        sendMessage(creds, chatID,{files: event.target.files,text:''})
+    }
+
+
+
+
+
     return(
         <div>
-            Message Form
+            <form className="message-form" onSubmit={handleSubmit}>
+                <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                />
+
+                <label htmlFor="upload-button">
+                    <span className="image-button">
+                        <PictureOutlined className="picture-icon"/>
+                    </span>
+                </label>
+                <input
+                    type="file"
+                    multiple={false}
+                    id="upload-button"
+                    style={{display: 'none'}}
+                    onChange={handleUpload.bind(this)}
+                />
+
+                <button type="submit" className="send-button">
+                    <SendOutlined className="send-icon"/>
+                </button>
+            </form>
         </div>
     )
 }
