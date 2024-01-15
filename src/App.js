76516ed29acbdc7c4c440f5431857b5920233b5f
Xavier Dunkley <xavierxv18@gmail.com>
2024-01-15 14:49:29 -0500

    created dashboard

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import Login from './components/Login';
 import useLocalStorage from './hooks/useLocalStorage';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
   const [id, setId] = useLocalStorage('id')
 
   return (
-    id ? <Login onIdSubmit={id}/> : <Login onIdSubmit={setId}/>
+    id ? <Dashboard id={id}/> : <Login onIdSubmit={setId}/>
   )
 }
 
