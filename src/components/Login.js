bb7d0beb981cf374a1aefb43d7d612773700fe88
Xavier Dunkley <xavierxv18@gmail.com>
2024-01-15 14:12:01 -0500

    completed login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
+import { v4 as uuidv4} from 'uuid'
 
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
@@ -8,6 +9,10 @@ export default function Login({ onIdSubmit }) {
         e.preventDefault()
 
         onIdSubmit(idRef.current.value)
+    }
+
+    function createNewId(){
+        onIdSubmit(uuidv4())
     }
 
 
@@ -19,7 +24,7 @@ export default function Login({ onIdSubmit }) {
                 <Form.Control type='text' ref={idRef} required/>
             </Form.Group>
             <Button type='submit' className="mr-2">Login</Button>    
-            <Button variant='secondary'>Create New ID</Button>
+            <Button onClick={createNewId} variant='secondary'>Create New ID</Button>
         </Form>
     </Container>  
     )
