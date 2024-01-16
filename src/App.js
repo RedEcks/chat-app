568d915769b8f7603b43d4e2a183a95c5a39e548
Xavier Dunkley <xavierxv18@gmail.com>
2024-01-15 22:20:44 -0500

    created contactsprovider and conversationsprovider

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React from 'react';
 import Login from './components/Login';
 import useLocalStorage from './hooks/useLocalStorage';
 import Dashboard from './components/Dashboard';
+import { ContactsProvider } from './contexts/ContactsProvider';
+import { ConversationsProvider } from './contexts/ConversationsProvider';
 
 function App() {
 
   const [id, setId] = useLocalStorage('id')
 
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id}/>
+      </ConversationsProvider>
+    </ContactsProvider>
+  )
+
   return (
-    id ? <Dashboard id={id}/> : <Login onIdSubmit={setId}/>
+    id ? dashboard: <Login onIdSubmit={setId}/>
   )
 }
 
